d517b66463c04d0657306c28d1ea9ed5734bb60b
Indre Surviliene <indre.bitinaite@gmail.com>
2022-03-16 19:43:12 +0200

    loop-Indre

--- Day 11 practical tasks/Program.cs
+++ Day 11 practical tasks/Program.cs
@@ -3,4 +3,18 @@
 Console.WriteLine(i);
 }
 
+Console.WriteLine("\n");
+
+string[] myArray = new string[5];
+
+myArray[0] = "one";
+myArray[1] = "two";
+myArray[2] = "three";
+myArray[3] = "four";
+myArray[4] = "five";
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+}
 
